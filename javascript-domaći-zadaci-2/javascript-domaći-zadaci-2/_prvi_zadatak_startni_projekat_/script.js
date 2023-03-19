ca7ae44689aac3a9de12e68493c1ee1b8cfff1f1
Aleksandar <109678271+Latial@users.noreply.github.com>
2023-03-19 15:30:41 +0100

    Add files via upload

---
+++ javascript-domaći-zadaci-2/javascript-domaći-zadaci-2/_prvi_zadatak_startni_projekat_/script.js
@@ -0,0 +1,17 @@
+// DOPUNITI
+// postaviti neophodne promenljive za rad sa HTML elementima
+
+
+// napomena: datum je u formatu 'MESEC.DAN.GODINA SATI:MINUTI:SEKUNDE', npr. new Date('12.16.2022 00:00:00')
+// DOPUNITI
+// postaviti odgovarajuće vreme za odbrojavanje
+movieReleaseTime = new Date('VREME IZLASKA FILMA U NAPOMENUTOM FORMATU')
+
+// DOPUNITI
+// napisati funkciju koja računa i zapisuje vreme odbrojavanja do filma unutar odgovarajućih HTML elemenata
+function updateCountdown() {
+  
+}
+
+// svake sekunde osvežavamo vrednosti na ekranu, odnosno pozivamo updateCountdown funkciju
+setInterval(updateCountdown, 1000)
